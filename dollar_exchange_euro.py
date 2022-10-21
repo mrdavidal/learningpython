8f0d58e4797eeb05fbe625be6dfdded52aedf372
mrdavidal <contis.ali@gmail.com>
2022-10-21 10:25:55 +0300

    exchange euro to usd and reverse

---
+++ dollar_exchange_euro.py
@@ -0,0 +1,8 @@
+dollar = 122
+euros = 66
+
+exchange_dollar = dollar * 1.02
+exchange_euro = euros * 0.98
+
+print(dollar,"dollars are", round(exchange_dollar, 2), "euros")
+print(euros,"euros are", round(exchange_euro, 2), "dollars")
